098688211a3f47d8735b66962f072a4c866e1a7f
YeomanLi <Yeoman_Li@Outlook.com>
2020-03-27 17:14:03 +0800

    :tada: (init) d
    dfssdf

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default () => {
         <Button>Go to About</Button>
       </Link>
       <Tag onClick={() => Router.push('/post/comment')}>Click To Comment</Tag>
+      <Tag onClick={() => Router.push('/post/comment')}>Click To Comment</Tag>
       <Link href="/post/[pid]" as="/post/first">
         <Button>Go to First Post</Button>
       </Link>
